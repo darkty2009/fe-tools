7c292249723bfc6e66b11ad00ba3544b156c95e2
lizihan <darkty2009@gmail.com>
2016-11-19 14:37:59 +0800

    something

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #webpack -p
 
 datestr=$(date +%Y%m%d%H%M)
+path=$(pwd)
 mkdir logs
 
 forever stopall
-NODE_ENV=production forever start -l ./logs/$datestr.log -e ./logs/$datestr.error -a server/index.js
+NODE_ENV=production forever start -l $path/logs/$datestr.log -e $path/logs/$datestr.error -a server/index.js
