691470112d063e019b9b3dfda94b8ec2cbabe5c3
lizihan <darkty2009@gmail.com>
2016-11-24 10:37:26 +0800

    change log

--- start.sh
+++ start.sh
@@ -4,5 +4,8 @@ datestr=$(date +%Y%m%d%H%M)
 path=$(pwd)
 mkdir logs
 
+mv .log $datestr.log
+mv .error $datestr.error
+
 forever stopall
-NODE_ENV=production forever start -l $path/logs/$datestr.log -e $path/logs/$datestr.error -a server/index.js
+NODE_ENV=production forever start -l $path/logs/.log -e $path/logs/.error -a server/index.js
