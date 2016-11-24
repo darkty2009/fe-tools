8337c3f5559095e353754b1266f80383435ebbfd
lizihan <darkty2009@gmail.com>
2016-11-24 10:40:50 +0800

    access and error

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ datestr=$(date +%Y%m%d%H%M)
 path=$(pwd)
 mkdir logs
 
-mv .log $datestr.log
-mv .error $datestr.error
+mv .log access_$datestr.log
+mv .error error_$datestr.log
 
 forever stopall
-NODE_ENV=production forever start -l $path/logs/.log -e $path/logs/.error -a server/index.js
+NODE_ENV=production forever start -l $path/logs/access.log -e $path/logs/error.log -a server/index.js
