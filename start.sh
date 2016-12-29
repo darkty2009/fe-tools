b38cdead4c4cb56e95e0849cf04c1beb797f6eb4
= <=>
2016-12-29 14:52:06 +0800

    logs

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
-#webpack -p
+webpack -p
 
 datestr=$(date +%Y%m%d%H%M)
 path=$(pwd)
 mkdir logs
 
-mv .log access_$datestr.log
-mv .error error_$datestr.log
+mv ./logs/access.log ./logs/access_$datestr.log
+mv ./logs/error.log ./logs/error_$datestr.log
 
 forever stopall
 NODE_ENV=production forever start -l $path/logs/access.log -e $path/logs/error.log -a server/index.js
